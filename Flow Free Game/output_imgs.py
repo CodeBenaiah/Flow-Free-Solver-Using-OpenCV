551d0cdfe0073cb5ee8bd4ee7e2f3ae445a9157e
Ben <ben@pop-os.localdomain>
2023-07-06 11:40:41 +0530

    Updates

--- Flow Free Game/output_imgs.py
+++ Flow Free Game/output_imgs.py
@@ -4,15 +4,6 @@ col_val = [[0,0,254,1],[0,141,2,2],[4,224,234,3],[254,41,14,4],[0,137,250,5]]
 
 path = "/home/ben/Code/Flow-Free-Solver-Using-OpenCV/Flow Free Game/5x5.jpg"
 
-img = cv2.imread(path)
-n = 5
-img = cv2.resize(img,(500, 500))
-
-output = [1,2,2,3,3,1,2,4,3,5,1,2,4,3,5,1,2,4,3,5,1,1,4,5,5]
-
-secw = int(img.shape[1]/n)
-sech = int(img.shape[0]/n)
-
 a = b = 25
 k = l = 75
 m=0
