cf9a0ad54913212ab954943e1781c9fd8717bae7
Ben <ben@pop-os.localdomain>
2023-07-15 15:27:18 +0530

    Some more changes

--- Flow Free Game/final_test.py
+++ Flow Free Game/final_test.py
@@ -63,12 +63,13 @@ def splitBoxes(img,n):
     print(box_dimension)
     return boxes
 
-col_val = [[0,0,254,1],[0,141,2,2],[4,224,234,3],[254,41,14,4],[0,137,250,5]]
+col_val = [[0,0,254,1],[0,141,2,2],[4,224,234,3],[254,41,14,4],[0,137,250,5],[35,51,234,6],[243,40,19,7],[74,224,231,8],[38,139,61,9]]
 
 def predict(boxes):
     board = []
     for image in boxes:
         col = (image[50,50])
+        print(col)
         e = 0
         for i in col_val:
             if i[0]==col[0] and i[1]==col[1] and i[2]==col[2]:
@@ -119,6 +120,7 @@ if biggest.size!= 0:
     imgWarpColored = cv2.warpPerspective(img, matrix, (width,height))
     imgDetectedCircles = imgBlank.copy()
 
+imgWarpColored = cv2.imread("5x5 Warped.png")
 imgSolvedDigits = imgBlank.copy()
 n = 5
 boxes = splitBoxes(imgWarpColored,n)
