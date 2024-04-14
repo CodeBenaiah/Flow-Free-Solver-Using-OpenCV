54a4b0fd63b7f21bbd0c6a6966b46979df7cebb2
Ben <benaiahsaju@gmail.com>
2024-04-14 20:47:55 +0530

    Updated main.py and removed previous working files

---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+Get the image
+extract grid -> returns grid
+extract matrix -> returns board 
+extract solution matrix -> return solved board
+send it to visualizer -> returns annotated image
+
+"""
+
+import os
+from optparse import OptionParser
+from .data_preprocessing import GridExtractor
+from .data_preprocessing import MatrixGenerator
+from .image_annotation import Visualizer
+
+def process_images_in_folder(folder_path):
+    """
+    Process all image files in the specified folder.
+
+    Args:
+        folder_path (str): The path to the folder containing image files.
+    """
+    files = os.listdir(folder_path)
+    image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+    if not image_files:
+        print("No image files found in the folder.")
+        return
+    
+    for image_file in image_files:
+        try:
+            image_path = os.path.join(folder_path, image_file)
+            print(f"Processing {image_path}")
+            grid_converter = GridExtractor(image_path)
+            grid_converter.preprocess_image()
+            extracted_img = grid_converter.extract_grid()
+            grid_converter.save_grid(extracted_img)
+            matrix_generator = MatrixGenerator(image_path)
+            color_dataset, row_size, col_size = matrix_generator.save_matrix(extracted_img)
+            os.system("./solution_algorithm/a.out")
+            visualizer = Visualizer(color_dataset, extracted_img, row_size, col_size)
+            visualizer.display_output()
+        except Exception:
+            print("Error faced: ", Exception)
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-f", "--folder", dest="folder_path", help="Path to the folder containing image files")
+    (options, args) = parser.parse_args()
+
+    if not options.folder_path:
+        parser.error("Path to the folder containing image files is required. Use -f or --folder option.")
+
+    process_images_in_folder(options.folder_path)
